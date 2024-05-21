20efb11212e53501ae57a4ca35c589cc6aad1499
Matheus Ramos de Pierro <matpierro570@gmail.com>
2024-05-21 13:30:12 -0300

    fix: set region server manually

--- src/main/java/com/matheuspierro/EksClusterCICD/config/AwsConfig.java
+++ src/main/java/com/matheuspierro/EksClusterCICD/config/AwsConfig.java
@@ -2,6 +2,7 @@ package com.matheuspierro.EksClusterCICD.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.eks.AmazonEKS;
 import com.amazonaws.services.eks.AmazonEKSClientBuilder;
 import io.github.cdimascio.dotenv.Dotenv;
@@ -16,11 +17,10 @@ public class AwsConfig {
         Dotenv dotenv = Dotenv.load();
         String awsAccessKey = dotenv.get("AWS_ACCESS_KEY_ID");
         String awsSecretKey = dotenv.get("AWS_SECRET_ACCESS_KEY");
-        String awsDefaultRegion = dotenv.get("AWS_DEFAULT_REGION");
 
         return AmazonEKSClientBuilder.standard()
                 .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(awsAccessKey, awsSecretKey)))
-                .withRegion(awsDefaultRegion)
+                .withRegion(Regions.SA_EAST_1)
                 .build();
     }
 }
